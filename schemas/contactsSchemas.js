da1509a92f0098367e9e1ab93d521804af5f3406
NikVio <131026216+NikVio@users.noreply.github.com>
2024-01-31 18:10:31 +0200

    contactsAPI

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,5 +1,40 @@
-// import Joi from "joi";
+const Joi = require("joi");
 
-// export const createContactSchema = Joi.object({});
+const createContactSchema = Joi.object({
+  name: Joi.string()
+    .trim()
+    .pattern(new RegExp("^[a-zA-Z ]+$"))
+    .min(2)
+    .max(30)
+    .required(),
 
-// export const updateContactSchema = Joi.object({});
+  email: Joi.string().trim().email().required(),
+
+  phone: Joi.string()
+    .trim()
+    .pattern(new RegExp("^\\(\\d{3}\\) \\d{3}-\\d{4}$"))
+    .min(14)
+    .max(14)
+    .required(),
+})
+  .min(3)
+  .max(3);
+
+const updateContactSchema = Joi.object({
+  name: Joi.string().trim().pattern(new RegExp("^[a-zA-Z ]+$")).min(2).max(30),
+
+  email: Joi.string().trim().email(),
+
+  phone: Joi.string()
+    .pattern(new RegExp("^\\(\\d{3}\\) \\d{3}-\\d{4}$"))
+    .trim()
+    .min(14)
+    .max(14),
+})
+  .min(1)
+  .max(3)
+  .messages({
+    "object.min": "Body must have at least one field",
+  });
+
+module.exports = { createContactSchema, updateContactSchema };

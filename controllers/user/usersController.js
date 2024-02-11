335d72dbdbdc3fb35dc91c861d26523f843be93c
NikVio <131026216+NikVio@users.noreply.github.com>
2024-02-11 03:50:17 +0200

    auth/modified

--- controllers/user/usersController.js
+++ controllers/user/usersController.js
@@ -1,28 +1,16 @@
 const controllerWrapper = require("../../helpers/controllersWrapper");
 const User = require("../../models/users");
 
-const requestError = require("../../helpers/HttpError");
-
 const logoutUser = async (req, res, next) => {
   const { _id } = req.user;
-  const user = await User.findByIdAndUpdate(_id, { token: "" });
+  await User.findByIdAndUpdate(_id, { token: "" });
 
-  if (!user) {
-    throw requestError(401, "Not authorized");
-  }
-
-  res.status(204).json({
-    message: "Logout succes",
-  });
+  res.status(204).json();
 };
 
 const currentUser = async (req, res, next) => {
   const { email, _id } = req.user;
-  const user = await User.findById(_id);
-
-  if (!user) {
-    throw requestError(401, "Not authorized");
-  }
+  await User.findById(_id);
 
   res.status(200).json({
     email,
